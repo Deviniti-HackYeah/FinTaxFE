c3a2c30d6d533a0842d4405e2fc09933fc1e686e
Maciej Hajduk <maciej.hajduk@deviniti.com>
2024-09-28 14:21:08 +0200

    feat: pwa added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { LucideAngularModule, icons } from 'lucide-angular';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from '@shared/shared.module';
 import { ApiModule } from '@api/api.module';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 const GLOBAL_MODULES = [LucideAngularModule.pick(icons)];
 
@@ -17,6 +18,12 @@ const GLOBAL_MODULES = [LucideAngularModule.pick(icons)];
     BrowserModule,
     SharedModule,
     ApiModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   bootstrap: [AppComponent],
 })
